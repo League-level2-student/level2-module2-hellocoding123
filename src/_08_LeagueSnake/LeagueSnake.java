da34b1b9e3d19a9bc84e80fd9fc13c086c2d42cf
lvl0_module2 <zoey.lestyk@gmail.com>
2023-12-02 12:58:59 -0800

    snake

--- src/_08_LeagueSnake/LeagueSnake.java
+++ src/_08_LeagueSnake/LeagueSnake.java
@@ -17,12 +17,13 @@ public class LeagueSnake extends PApplet {
     int segment;
     int foodX;
     int foodY;
+    int currentFoodX;
+    int currentFoodY;
     int headX = 260;
     int headY= 60;
     int direction=UP;
     int foodEaten;
-    int tailX;
-    int tailY;
+
     ArrayList<Segment>tail= new ArrayList();
     
     /*
@@ -44,11 +45,27 @@ public class LeagueSnake extends PApplet {
 
     void dropFood() {
         // Set the food in a new random location
-    	foodX = ((int)random(25)*20);
-    	foodY = ((int)random(25)*20);
+    	do {
+	    	foodX = ((int)random(25)*20);
+	    	foodY = ((int)random(25)*20);
+    	}
+    	while (foodNotOnSnake());
     }
 
-    /*
+    private boolean foodNotOnSnake() {
+		// TODO Auto-generated method stub
+    	if(foodX == headX && foodY == headY) {
+    		return false;
+    	}
+    	for(int i = 0; i < tail.size(); i++) {
+    		if(tail.get(i).x == foodX && tail.get(i).y == foodY) {
+    			return false;
+    		}
+    	}
+    	return true;
+	}
+
+	/*
      * Draw Methods
      * 
      * These methods are used to draw the snake and its food
@@ -65,14 +82,18 @@ public class LeagueSnake extends PApplet {
 
     void drawFood() {
         // Draw the food
+    	currentFoodX = foodX;
+    	currentFoodY = foodY;
     	fill(255,0,0);
     	noStroke();
-        rect(foodX,foodY,20,20);
+        rect(currentFoodX,currentFoodY,20,20);
+        fill(165,42,42);
+        rect(currentFoodX + 9, currentFoodY - 4, 2, 4);
     }
 
     void drawSnake() {
         // Draw the head of the snake followed by its tail
-    	fill(0,255, 0);
+    	fill(0,200, 0);
     	noStroke();
     	rect(headX,headY,20,20);
     	manageTail();
@@ -80,7 +101,9 @@ public class LeagueSnake extends PApplet {
 
     void drawTail() {
         // Draw each segment of the tail
-        rect(tailX,tailY,20,20);
+        for(int i = 0; i < tail.size(); i++) {
+        	rect(tail.get(i).x, tail.get(i).y, 20,20);
+        }
     }
 
     /*
@@ -93,26 +116,22 @@ public class LeagueSnake extends PApplet {
     	checkTailCollision();
     	drawTail();
         // After drawing the tail, add a new segment at the "start" of the tail and
-        tailX=headX;
-        tailY=headY;
-    	Segment tailSegment = new Segment(tailX, tailY);
+    	Segment tailSegment = new Segment(headX, headY);
     	tail.add(tailSegment);
         // remove the one at the "end"
-    	tail.remove(tail.size()); 
+    	tail.remove(0); 
         // This produces the illusion of the snake tail moving.
 
     }
 
     void checkTailCollision() {
         // If the snake crosses its own tail, shrink the tail back to one segment
-        for(int i = 0; i >= tail.size(); i++) {
+        for(int i = 0; i < tail.size(); i++) {
             if(tail.get(i).x == headX && tail.get(i).y == headY) {
                 foodEaten=1;
                 tail.clear();
-                tailX=headX;
-                tailY=headY;
-        	    Segment tailSegment = new Segment(tailX, tailY);
-        	    tail.add(tailSegment);
+        	   // Segment tailSegment = new Segment(tail.get(tail.size()-1).x, tail.get(tail.size()-1).y);
+        	    //tail.add(tailSegment);
                 break;
             }    
         }    
@@ -128,7 +147,16 @@ public class LeagueSnake extends PApplet {
     public void keyPressed() {
         // Set the direction of the snake according to the arrow keys pressed
         System.out.println(keyCode);
-        direction = keyCode;
+        
+        if(
+          (direction == UP && keyCode != 40) || 
+          (direction == DOWN && keyCode != 38) || 
+          (direction == LEFT && keyCode != 39) ||
+          (direction == RIGHT && keyCode != 37)
+          )
+        {
+        	direction = keyCode;
+        }
     }
 
     void move() {
@@ -171,9 +199,7 @@ public class LeagueSnake extends PApplet {
     	if(headX == foodX && headY == foodY) {
         // When the snake eats the food, its tail should grow and more
     		foodEaten += 1;
-            tailX=headX;
-            tailY=headY;
-            Segment tailSegment = new Segment(tailX, tailY);
+            Segment tailSegment = new Segment(headX, headY);
             tail.add(tailSegment);
         // food appear
     		foodX = ((int)random(25)*20);
